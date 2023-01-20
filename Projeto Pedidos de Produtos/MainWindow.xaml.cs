01095402c5ab5e1692201d7b137ac1fb6b86d5c5
20201011110030 <20201011110030@CNAT407162>
2023-01-20 15:45:41 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Pedidos de Produtos/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projeto_Pedidos_de_Produtos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Pedido_Click(object sender, RoutedEventArgs e)
+        {
+            PedidoWindow pedido = new PedidoWindow();
+            pedido.ShowDialog();
+        }
+    }
+}
